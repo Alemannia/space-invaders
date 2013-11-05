5ffb047161fe923307ef1d95879d744e44802cda
Felix <felix.reidl@gmail.com>
2013-11-05 13:30:44 +0100

    Changed code slighlty to use new lib feature of getting the dimension of a sprite

--- src/lufti/invaders/InvaderGame.java
+++ src/lufti/invaders/InvaderGame.java
@@ -14,43 +14,45 @@ import lufti.ui.Canvas;
 
 /**
  * The invader game.
+ *
  * @author ubik
  */
 public class InvaderGame extends AbstractGame {
-	
+
 	private SpriteSheet sprites;
-	private int x,y;
+	private int x, y;
 	private ArrayList<Point> bullets = new ArrayList<>();
-	
+
 	public InvaderGame() throws IOException {
 		sprites = SpriteSheetFactory.getClassic();
 		x = 50;
 		y = 500;
 	}
-	
+
 	@Override
 	public void update(PlayerInput input) {
-		if(input.containsCommand(Command.RIGHT)) {
+		if (input.containsCommand(Command.RIGHT)) {
 			x += 8;
-			if( x > 750) {
+			if (x > 750) {
 				x = 750;
 			}
-		} 
-		if(input.containsCommand(Command.LEFT)) {
+		}
+		if (input.containsCommand(Command.LEFT)) {
 			x -= 8;
-			if( x < 50) {
+			if (x < 50) {
 				x = 50;
 			}
-		} 
-		
-		if(input.containsCommand(Command.SHOOT)) {
-			bullets.add(new Point(x-2,y-12));
+		}
+
+		int projHeight = sprites.getSpriteDimension("ProjectileA", 0).height;
+		if (input.containsCommand(Command.SHOOT)) {
+			bullets.add(new Point(x - 2, y - projHeight));
 		}
 
 		ArrayList<Point> keep = new ArrayList<>();
- 		for (Point point : bullets) {
+		for (Point point : bullets) {
 			point.y -= 8;
-			if( point.y > -50) {
+			if (point.y > -50) {
 				keep.add(point);
 			}
 		}
@@ -59,13 +61,14 @@ public class InvaderGame extends AbstractGame {
 
 	@Override
 	public void render(Canvas.CanvasPainter pntr) {
-		BufferedImage invader = sprites.getSprite("Ship", 0);
-		pntr.drawImage(invader, x - invader.getWidth()/2, y);
-		
+		BufferedImage ship = sprites.getSprite("Ship", 0);
+		int width = sprites.getSpriteDimension("Ship", 0).width;
+		pntr.drawImage(ship, x - width / 2, y);
+
 		BufferedImage proj = sprites.getSprite("ProjectileA", 0);
 		for (Point point : bullets) {
 			pntr.drawImage(proj, point.x, point.y);
 		}
 	}
-	
+
 }
